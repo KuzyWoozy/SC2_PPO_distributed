4d1ca314fa80cee8730e0ea0e2514770204b2a79
s2402307 <s2402307@ed.ac.uk>
2023-08-14 20:16:40 +0100

    Fixed local install script

--- scripts/install_local.sh
+++ scripts/install_local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#wget https://blzdistsc2-a.akamaihd.net/Linux/SC2.4.9.3.zip
-#unzip SC2.4.9.3.zip
-#chmod a+x SC2.4.9.3/StarCraftII/Versions/Base75025/SC2_x64
-#chmod a+x evaluate.py train.py
+wget https://blzdistsc2-a.akamaihd.net/Linux/SC2.4.9.3.zip
+unzip SC2.4.9.3.zip
+chmod a+x SC2.4.9.3/StarCraftII/Versions/Base75025/SC2_x64
+chmod a+x evaluate.py train.py
 
 python3.9 -m venv sc_virt
 source sc_virt/bin/activate
